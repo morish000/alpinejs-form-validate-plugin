397e7b5faecdec49070aa0e21d91d22b96f98e2f
root <root@nono.localdomain>
2024-10-12 04:23:48 +0900

    add build task

---
+++ build.ts
@@ -0,0 +1,49 @@
+import { build, type Plugin, stop } from "esbuild";
+
+const processCache = async (entryPoints: string[]) => {
+  const command = new Deno.Command("deno", {
+    args: ["cache", ...entryPoints],
+    stdout: "piped",
+    stderr: "piped",
+  });
+
+  const { code, stdout, stderr } = await command.output();
+
+  const decoder = new TextDecoder();
+
+  if (code !== 0) {
+    console.error(decoder.decode(stderr));
+    Deno.exit(code);
+  }
+
+  console.log(decoder.decode(stdout));
+};
+
+const denoCachePlugin = (entryPoints: string[]): Plugin => ({
+  name: "deno-cache-plugin",
+  setup(build: { onStart: (arg0: () => Promise<void>) => void }) {
+    build.onStart(async () => {
+      await processCache(entryPoints);
+    });
+  },
+});
+
+const entryPoints: string[] = [
+  `src/mod.ts`,
+  `src/functions/index.ts`,
+  `src/i18next/alpinejs_i18next_plugin.ts`,
+  `src/i18next/i18next_message_resolver.ts`,
+];
+
+await build({
+  entryPoints,
+  outdir: "dist",
+  format: "esm",
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  resolveExtensions: [".ts"],
+  plugins: [denoCachePlugin(entryPoints)],
+});
+
+stop();
---
+++ dist/i18next/i18next_message_resolver.js
@@ -0,0 +1,2 @@
+var o=i=>r=>{let n=i.i18next(),t=[];return r.watch(()=>i.timestamp,()=>t.forEach(e=>e())),{addUpdateListener:e=>{t.includes(e)||t.push(e)},removeUpdateListener:e=>{t=t.filter(s=>s!==e)},resolve:(...e)=>{let[s,p]=e;return n.exists(s)?n.t(s,p):s}}};export{o as createI18NextMessageResolver};
+//# sourceMappingURL=i18next_message_resolver.js.map
